7fca878358d5466ae9fbf1bf1230bf3df7de6862
Kerry Lang <klang812@gmail.com>
2021-01-14 13:27:48 -0800

    fix scripts refresh screen with different conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-  if (parseInt("input#age" >= 18)) {
-    $('#voting-page').show();
-  } else {
-    $('#under-18').show();
-  }
+
+  $("form#ourForm").submit(function(e){
+    e.preventDefault();
+
+    const age = parseInt($("input#age").val());
+  
+    if (age >= 18) {
+      $('#voting-page').show();
+      $('#under-18').hide();
+    } else {
+      $('#under-18').show();
+      $('#voting-page').hide();
+    } 
+  });
+  
   
 });
